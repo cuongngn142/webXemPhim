4a28ec1fc67ec0f4ce58444b68941ac5868d68e0
Cuong1402 <cuongngn.tech@gmail.com>
2025-11-11 21:48:03 +0700

    Update episodeService.js

--- frontend/services/episodeService.js
+++ frontend/services/episodeService.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://website-xem-phim.onrender.com";
+
 
 export async function fetchEpisodesByMovieId(movieId) {
   const res = await fetch(`${API_URL}/api/movies/${movieId}/episodes`);
