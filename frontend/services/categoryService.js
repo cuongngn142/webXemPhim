9952fdcca6fe579cd59592ff13be7ca258d72cdc
Cuong1402 <cuongngn.tech@gmail.com>
2025-11-11 21:43:40 +0700

    Update categoryService.js

--- frontend/services/categoryService.js
+++ frontend/services/categoryService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000"; 
+const API_URL = "https://website-xem-phim.onrender.com"; 
 
 export async function fetchAllCategories() {
   const res = await fetch(`${API_URL}/api/categories`);
