c65b19d42aacf78eeacdba02d6e6a52c6bfe5e06
Cuong1402 <cuongngn.tech@gmail.com>
2025-11-11 22:08:05 +0700

    Update Movies.js

--- frontend/pages/Movies.js
+++ frontend/pages/Movies.js
@@ -6,7 +6,7 @@ import {
   updateMovieCategories,
 } from "../services/movieService.js";
 import { fetchAllCategories } from "../services/categoryService.js";
-const API_URL = "http://localhost:3000";
+const API_URL = "https://website-xem-phim.onrender.com";
 
 export function renderMoviePage() {
   return `
